ac54173fe1a91579afddfbcc4e875588b4d5db7e
Georg Hartmann <georg@zahlz.com>
2016-11-15 23:55:54 +0100

    Bump package and examples to 0.18

--- examples/SignupForm.elm
+++ examples/SignupForm.elm
@@ -1,26 +1,34 @@
-module SignupForm where
+module SignupForm exposing (..)
 
 import Validate exposing (..)
 
 
 -- Example Using Library --
 
-type alias Model = { name : String, email : String, age : String }
 
-type Field = Name | Email | Age
+type alias Model =
+    { name : String, email : String, age : String }
 
 
-validateModel : Model -> List (Field, String)
+type Field
+    = Name
+    | Email
+    | Age
+
+
+validateModel : Model -> List ( Field, String )
 validateModel =
     Validate.all
-        [ .name  >> ifBlank (Name => "Please enter a name.")
+        [ .name >> ifBlank (Name => "Please enter a name.")
         , .email >> ifBlank (Email => "Please enter an email address.")
-        , .age   >> ifNotInt (Age => "Age must be a whole number.")
+        , .age >> ifNotInt (Age => "Age must be a whole number.")
         ]
 
 
 result =
     validateModel { name = "Richard", email = "", age = "abc" }
-        == [(Email, "Please enter an email address."), (Age, "Age must be a whole number.")]
+        == [ ( Email, "Please enter an email address." ), ( Age, "Age must be a whole number." ) ]
 
-(=>) = (,)
+
+(=>) =
+    (,)
